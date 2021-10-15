ea6cbb29ddf453b82453729506c60232c1b39693
unknown <restuyogasmk7@gmail.com>
2021-10-15 16:57:03 +0700

    modal add

--- src/components/view/Category.vue
+++ src/components/view/Category.vue
@@ -10,6 +10,14 @@
               <div class="card">
                 <div class="card-header">
                   <h3 class="card-title">Data Category</h3>
+                   <div class="card-tools">
+                    <button
+                        type="submit"
+                        class="btn btn-success"
+                        data-toggle="modal" data-target="#exampleModal">
+                        Add Category
+                    </button>
+                </div>
                 </div>
                 <!-- /.card-header -->
                 <div class="card-body">
@@ -43,6 +51,55 @@
           <!-- /.row -->
         </div>
         <!-- /.container-fluid -->
+                <!-- Modal -->
+       <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5
+                            class="modal-title"
+                            id="exampleModalLongTitle"
+                        >
+                            Add Category
+                        </h5>
+                        <button
+                            type="button"
+                            class="close"
+                            data-dismiss="modal"
+                            aria-label="Close"
+                        >
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <form>
+                        <div class="modal-body">
+                            <div class="form-group">
+                                <input
+                                    type="text"
+                                    class="form-control"
+                                    placeholder="Category Name"
+                                />
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button
+                                type="button"
+                                class="btn btn-secondary"
+                                data-dismiss="modal"
+                            >
+                                Close
+                            </button>
+                            <button
+                                type="submit"
+                                class="btn btn-primary"
+                            >
+                                Save
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
       </section>
     </div>
     <foot-bar></foot-bar>
@@ -59,6 +116,6 @@ export default {
     NavBar,
     SideBar,
     FootBar
-    }
+    },
 }
 </script>
